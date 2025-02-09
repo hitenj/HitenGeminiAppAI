a1383b71ecc707a9ee479fab988cd2e733c38854
Hiten <hgeetaj@gmail.com>
2025-02-09 17:19:37 +0530

    Demo

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const [question, setQuestion] = useState('');
+  const [response, setResponse] = useState('');
+
+const submitHandler = (e)=>{
+    e.preventDefault();
+    console.log(question)
+    axios.post('https://hiten-gemini-app.vercel.app/getResponse', {
+      question:question
+    })
+    .then(res=>{
+      console.log(res.data.response);
+      setResponse(res.data.response);
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+}
+
+const speakHandler = () =>{
+  const a = new SpeechSynthesisUtterance(response)
+  window.speechSynthesis.speak(a)
+}
+
+  return (
+    <div className="App">
+      <div className="box">
+        <div className='profile-pic'>
+          <img className='pic' alt='profile pic' src={require('../src/assets/0D5A7584.JPG')} />
+        </div>
+        <p className="label">Hiten Jain</p>
+        <textarea onChange={(e)=> {
+            setQuestion(e.target.value)
+        }}/>
+        <button onClick={submitHandler}>Send</button>
+      </div>
+      <div className="box">
+        <div className='profile-pic'>
+          <img className='pic' alt='profile pic' src={require('../src/assets/gemini.webp')} />
+        </div>
+        <p className="label">Gemini</p>
+        <textarea value={response}/>
+        <button onClick={speakHandler}>Speak <i class="fa fa-microphone"></i></button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
